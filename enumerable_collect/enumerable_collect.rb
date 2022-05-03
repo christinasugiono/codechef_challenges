1b66a97b4d56b012e5e43c89a834b0c6fed867de
Christina Sugiono <christina.sugiono95@gmail.com>
2022-05-03 10:13:29 +0800

    enumerable collect ROT13 decoding

--- enumerable_collect/enumerable_collect.rb
+++ enumerable_collect/enumerable_collect.rb
@@ -1,3 +1,11 @@
 def rot13(secret_messages)
   # your code here
+    alphabets = ('a'..'z').to_a
+    secret_messages.map! do |message|
+        decode = message.chars.collect do |char|
+            char == " " ? " " : alphabets[alphabets.index(char.downcase) - 13]
+        end
+        decode.join
+    end
+    return secret_messages
 end
